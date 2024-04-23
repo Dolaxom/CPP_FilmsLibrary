39fb36ad75e238bb00ab493b7a037f6d99351c17
Nikolay Ryzhov <nikolairyzhov@transnetsoft.com>
2024-04-23 11:54:32 +0300

    [logger] Fixing code-style

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -10,23 +10,23 @@ namespace fm {
 
 class Logger {
  public:
-    void Write(std::string_view info) {
-        std::ofstream logFile{fs::path{logDirName}, std::ios::app};
-        if (!logFile.is_open()) {
-            std::cerr << "!logFile.is_open()" << std::endl;
-            return;
-        }
-
-        logFile << info << "\n";
-        std::cout << info << "\n";
-
-        logFile.close();
+  void Write(std::string_view info) {
+    std::ofstream logFile{fs::path{logDirName}, std::ios::app};
+    if (!logFile.is_open()) {
+      std::cerr << "!logFile.is_open()" << std::endl;
+      return;
     }
-    
-    static Logger Get() {
-        static Logger logger;
-        return logger;
-    }
+
+    logFile << info << "\n";
+    std::cout << info << "\n";
+
+    logFile.close();
+  }
+
+  static Logger Get() {
+    static Logger logger;
+    return logger;
+  }
 
  private:
   Logger() = default;
