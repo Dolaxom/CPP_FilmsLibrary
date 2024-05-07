4a2a9b0d9024d705155efdad3e24e5b8da7d627c
Nikolay Ryzhov <nikolairyzhov@transnetsoft.com>
2024-05-07 14:47:04 +0300

    [tests] Adding unit-tests for utils/utils.h methods

--- src/tests/tests.cc
+++ src/tests/tests.cc
@@ -23,6 +23,22 @@ TEST(Database, 1) {
   EXPECT_EQ(res, true);
 }
 
+TEST(Util, 1) {
+  auto actor = Utils::JsonBodyToActor(R"({ "name" : "a1", "gender" : "gen", "date" : "1010" })");
+  EXPECT_EQ(actor.name, "a1");
+  EXPECT_EQ(actor.gender, "gen");
+  EXPECT_EQ(actor.date, "1010");
+}
+
+TEST(Util, 2) {
+  auto film = Utils::JsonBodyToFilm(R"({ "title" : "film", "desription" : "---", "date" : "1-2-3", "rating" : "10", "actors" : "{1, 3}" })");
+  EXPECT_EQ(film.title, "film");
+  EXPECT_EQ(film.description, "---");
+  EXPECT_EQ(film.release_date, "1-2-3");
+  EXPECT_EQ(film.rating, "10");
+  EXPECT_EQ(film.ids, "{1, 3}");
+}
+
 bool runGTests() {
   ::testing::InitGoogleTest();
   return RUN_ALL_TESTS();
